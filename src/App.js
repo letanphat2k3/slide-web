11c2b5009298ce1757b6b755789853caac68dbda
letanphat2k3 <ltpht24022k3@gmail.com>
2024-10-24 00:36:52 +0700

    Feat:upload secondary - slide web

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-// App.js
 import React from 'react';
-import './App.css';
-import { Slide } from './component/Slide/Slide';
-import { Button } from './component/Button/Button';
-import { useSlide } from './hook/useSlide';
+import Slide from './component/Slide/Slide';
 
 function App() {
-  const images = [
-    "https://picsum.photos/300/200?image=0",
-    "https://picsum.photos/300/200?image=1",
-    "https://picsum.photos/300/200?image=2",
-    "https://picsum.photos/300/200?image=3",
-    "https://picsum.photos/300/200?image=4",
-  ];
-
-  const { currentIndex, nextSlide, prevSlide, restartSlide } = useSlide(images);
-
   return (
     <div className="App">
-      <Slide currentImage={images[currentIndex]} />
-      <div className="button-group">
-        <Button onClick={prevSlide}>Previous</Button>
-        <Button onClick={nextSlide}>Next</Button>
-        <Button onClick={restartSlide}>Restart</Button>
-      </div>
+      <Slide />
     </div>
   );
 }
--- src/component/Button/Button.js
+++ src/component/Button/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Button.css';
 
-export const Button = ({ onClick, children }) => {
+export const Button = ({ onClick, children, className = 'btn' }) => {
   return (
-    <button onClick={onClick} className="btn">
+    <button onClick={onClick} className={className}>
       {children}
     </button>
   );
